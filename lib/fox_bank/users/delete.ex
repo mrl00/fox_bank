4f46470a7aaa890a6759bd130722c4296690342c
mrl00 <m2l_0@outlook.com>
2024-06-07 19:10:32 -0300

    feat: create delete calls

---
+++ lib/fox_bank/users/delete.ex
@@ -0,0 +1,11 @@
+defmodule FoxBank.Users.Delete do
+  alias FoxBank.Users.User
+  alias FoxBank.Repo
+
+  def call(id) do
+    case Repo.get(User, id) do
+      nil -> {:error, :not_found}
+      user -> Repo.delete(user)
+    end
+  end
+end
